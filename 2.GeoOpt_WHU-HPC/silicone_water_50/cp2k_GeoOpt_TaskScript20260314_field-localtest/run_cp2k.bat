0362bae799e9d62cb7a4fbb7f030b3508e0c6578
Siqi Tang <165524145+ResearchGeekSQ@users.noreply.github.com>
2026-05-09 20:55:34 +0800

    chore: clean up obsolete local and intermediate files in GeoOpt and AIMD directories

--- 2.GeoOpt_WHU-HPC/silicone_water_50/cp2k_GeoOpt_TaskScript20260314_field-localtest/run_cp2k.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting CP2K GEO_OPT with electric field ...
-docker compose -f docker-compose-cp2k.yml up --abort-on-container-exit
-echo.
-echo CP2K calculation finished. Check geo_opt.log for results.
-pause
